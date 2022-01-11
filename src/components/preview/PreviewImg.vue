c18ead7f8e1babf0c6673b7b7f03233d05ccf80f
Dongsheng Li <Lds010@outlook.com>
2022-01-11 19:19:35 +0800

    Update PreviewImg.vue

--- src/components/preview/PreviewImg.vue
+++ src/components/preview/PreviewImg.vue
@@ -1,6 +1,6 @@
 <template>
   <div className="Echarts">
-    <div id="main" :style="style"></div>
+    <div id="main" style="width: auto;height: 800px;"></div>
   </div>
 </template>
 
@@ -9,78 +9,37 @@ export default {
   name: 'PreviewImg',
   data() {
     return {
-      chart: ''
-    }
-  },
-  props: {
-    id: {
-      type: String
-    },
-    width: {
-      type: String,
-      default: "100%"
-    },
-    height: {
-      type: String,
-      default: "800px"
-    },
-    option: {
-      type: Object,
-      default() {
-        return {
-          title: {
-            text: this.$store.getters.getTitle,
-          },
-          tooltip: {},
-          legend: {
-            data: ['销量']
-          },
-          xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-          },
-          yAxis: {},
-          series: [{
-            name: '销量',
-            type: 'pie',
-            data: [5, 20, 36, 10, 10, 20]
-          }]
-        }
-      }
+      title: 'Echarts入门示例'
     }
   },
   methods: {
-    init() {
-      this.chart = this.$echarts.init(document.getElementById('main'));
-      this.chart.setOption(this.option);
-      window.addEventListener("resize", this.chart.resize);
-    }
-  },
-  computed: {
-    style() {
-      return {
-        height: this.height,
-        width:  this.width
-      }
-    }
-  },
-  watch: {
-    option: {
-      handler(newVal, oldVal) {
-        if (this.chart) {
-          if (newVal) {
-            this.chart.setOption(newVal);
-          } else {
-            this.chart.setOption(oldVal);
-          }
-        } else {
-          this.init();
-        }
-      },
-      deep: true //对象内部属性的监听，关键。
+    myEcharts() {
+      var myChart = this.$echarts.init(document.getElementById('main'));
+      //配置图表
+      var option = {
+        title: {
+          text: this.title,
+        },
+        tooltip: {},
+        legend: {
+          data: ['销量']
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      };
+      myChart.setOption(option);
+
     }
   },
   mounted() {
-    this.init();
+    this.myEcharts();
   }
 }
 </script>

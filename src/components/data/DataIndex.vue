74c0b1e0f9031282f45e36eb0ef99169a74e9e9b
Dongsheng Li <Lds010@outlook.com>
2022-01-28 11:37:17 +0800

    Update DataIndex.vue

--- src/components/data/DataIndex.vue
+++ src/components/data/DataIndex.vue
@@ -12,26 +12,26 @@
         :label="item.lable"
         width="180">
         <template  slot-scope="scope">
-          <p v-if="scope.column.property=='attr0'">{{scope.row[item.prop]}}</p>
+          <el-input v-if="scope.column.property=='attr0'" v-model="scope.row[item.prop]" placeholder=""></el-input>
           <el-input class="kuang" v-else v-model="scope.row[item.prop]" placeholder=""></el-input>
         </template>
       </el-table-column>
     </el-table>
     <!-- 原生写的编辑表格 -->
-    <table border="1px solid" cellspacing="1" cellpadding="0">
-      <tbody>
-      <tr>
-        <th v-for="(item,index) in keys" :key="index">{{item.lable}}</th>
-      </tr>
-      <tr class="active" v-for="(item,index) in tableData" :key="index">
-        <td v-for="(item1,index1) in Object.keys(item)" :key="index1">
-          <p v-if="index1==0">{{item[item1]}}</p>
-          <input v-else type="text" v-model="item[item1]"/>
-        </td>
-      </tr>
-      </tbody>
-      <button>提交</button>
-    </table>
+<!--    <table border="1px solid" cellspacing="1" cellpadding="0">-->
+<!--      <tbody>-->
+<!--      <tr>-->
+<!--        <th v-for="(item,index) in keys" :key="index">{{item.lable}}</th>-->
+<!--      </tr>-->
+<!--      <tr class="active" v-for="(item,index) in tableData" :key="index">-->
+<!--        <td v-for="(item1,index1) in Object.keys(item)" :key="index1">-->
+<!--          <p v-if="index1==0">{{item[item1]}}</p>-->
+<!--          <input v-else type="text" v-model="item[item1]"/>-->
+<!--        </td>-->
+<!--      </tr>-->
+<!--      </tbody>-->
+<!--      <button>提交</button>-->
+<!--    </table>-->
   </div>
 </template>
 
@@ -43,14 +43,14 @@ export default {
     return {
       // ----------------------
       keys:[
-        {lable:"一级机构",prop:"attr0"},
-        {lable:"本月入职人数",prop:"attr1"},
-        {lable:"本月离职人数",prop:"attr2"},
+        {lable:"yAxis\\xAxis",prop:"attr0"},
+        {lable:"Column1",prop:"attr1"},
+        {lable:"Column2",prop:"attr2"},
       ], //表头
       tableData:[
-        {attr0:"泰康人寿",attr1:null,attr2:null},
-        {attr0:"泰康养老",attr1:9,attr2:5},
-        {attr0:"泰康在线",attr1:3,attr2:7}
+        {attr0:"y1",attr1:null,attr2:null},
+        {attr0:"y2",attr1:null,attr2:null},
+        {attr0:"y3",attr1:null,attr2:null}
       ],//表体
     }
   },

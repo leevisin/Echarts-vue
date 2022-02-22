13853f683a499bab60849b9d9a19db6644c9a870
Dongsheng Li <Lds010@outlook.com>
2022-02-22 11:48:33 +0800

    Achieve code format by js_beautify
    在chart\Chart中实现了代码的格式化，可以整合到后面了

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import { js_beautify, css_beautify, html_beautify } from 'js-beautify'
 export default {
   data() {
     return {
@@ -25,7 +26,12 @@ export default {
     },
   },
   created() {
-    console.info(this.scriptStr);
+    let str = 'option = {  title: {    text: \'Referer of a Website\',    subtext: \'Fake Data\',    left: \'center\'  },  tooltip: {    trigger: \'item\'  },  legend: {    orient: \'vertical\',    left: \'left\'  },  series: [    {      name: \'Access From\',      type: \'pie\',      radius: \'50%\',      data: [        { value: 1048, name: \'Search Engine\' },        { value: 735, name: \'Direct\' },        { value: 580, name: \'Email\' },        { value: 484, name: \'Union Ads\' },        { value: 300, name: \'Video Ads\' }      ],      emphasis: {        itemStyle: {          shadowBlur: 10,          shadowOffsetX: 0,          shadowColor: \'rgba(0, 0, 0, 0.5)\'        }      }    }  ]};'
+    let strTest = js_beautify(str, {
+      indent_size: 2,
+      space_in_empty_paren: true
+    })
+    console.log(strTest);
   }
 }
 </script>

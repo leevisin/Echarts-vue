0434698a328409c03f24cb66ae47fcfe49c46b26
Dongsheng Li <Lds010@outlook.com>
2022-03-29 20:38:33 +0800

    Add jexcel and delete part css about height for warning

--- src/components/user/UserIndex.vue
+++ src/components/user/UserIndex.vue
@@ -1,82 +1,168 @@
 <template>
   <div>
-    Test
+    <el-upload
+      ref="input"
+      action="/"
+      :show-file-list="false"
+      :auto-upload="false"
+      :on-change="importExcel"
+      type="file"
+    >
+      <el-button
+        slot="trigger"
+        icon="el-icon-upload"
+        size="small"
+        type="primary">
+        Upload
+      </el-button>
+      <el-button
+        icon="el-icon-download"
+        size="small"
+        type="primary"
+        @click="exportToExcel">
+        Download
+      </el-button>
+    </el-upload>
+    <el-table style="width: auto" border :data="tableData" id="tableData">
+      <template v-for="(item,index) in tableHead">
+        <el-table-column :key="index" :prop="item.column_name" :label="item.column_comment"
+                         v-if="item.column_name !== 'id'">
+        </el-table-column>
+      </template>
+    </el-table>
   </div>
-<!--  <div class="screen-container">-->
-<!--    &lt;!&ndash;      <div class="screen-header">&ndash;&gt;-->
-<!--    &lt;!&ndash;        <ace :option="scriptStr"></ace>&ndash;&gt;-->
-<!--    &lt;!&ndash;      </div>&ndash;&gt;-->
-<!--    <div class="screen-body">-->
-<!--      <div ref="chartRef" style="width: 500px;height: 400px;" @sendOption=getOption></div>-->
-<!--    </div>-->
-<!--  </div>-->
-  <!--    <div>-->
-  <!--      <div id="components-demo">-->
-  <!--        <button-counter></button-counter>-->
-  <!--        <button-counter></button-counter>-->
-  <!--        <button-counter></button-counter>-->
-  <!--        <button-counter></button-counter>-->
-  <!--      </div>-->
-  <!--      <div>-->
-  <!--        <blog-post title="My journey with Vue"></blog-post>-->
-  <!--        <blog-post title="Blogging with Vue"></blog-post>-->
-  <!--        <blog-post title="Why Vue is so fun"></blog-post>-->
-  <!--      </div>-->
-  <!--      <blog-post-->
-  <!--        v-for="post in posts"-->
-  <!--        v-bind:key="post.id"-->
-  <!--        v-bind:title="post.title"-->
-  <!--      ></blog-post>-->
-  <!--    <ace value="Test"></ace>-->
-  <!--    </div>-->
+
 </template>
 
 <script>
+import XLSX from "xlsx";
+import FileSaver from 'file-saver'
 export default {
-  name: 'UserIndex',
+  name: "FillShippingCost",
+
   data() {
     return {
-      title: 'Echarts入门示例'
+// 表头数据
+      tableHead: [],
+// 表格数据
+      tableData: [],
     }
   },
+  //data结束
   methods: {
-    initChart() {
-      let myChart = this.$echarts.init(this.$refs.chartRef);
-      //配置图表
-      let option = {
-        title: {
-          text: this.$store.getters.getTitle,
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      };
-      myChart.setOption(option)
-      // Save Instance
-      this.chartInstance = myChart
-      // Save option into chartOption, and chartOption is an object
-      this.chartOption = option
+    //导入
+    importExcel(file) {
+      const types = file.name.split(".")[1];
+      const fileType = ["xlsx", "xlc", "xlm", "xls", "xlt"].some(
+        item => item === types
+      );
+      if (!fileType) {
+        alert("格式错误！请重新选择");
+        return;
+      }
+      this.file2Xce(file).then(tabJson => {
+        if (tabJson && tabJson.length > 0) {
+          const data = {};
+          const this_ = this;
+          // let params = Object.assign({}, this_.reviewForm);
+          this_.card = JSON.stringify(tabJson[0].sheet);
+          data.card = this_.card;            //中英文转化
+          this_.excelToTable(data);//调用生成table方法
+        }
+      });
+    },
+    file2Xce(file) {
+      return new Promise(function (resolve) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const data = e.target.result;
+          this.wb = XLSX.read(data, {
+            type: "binary"
+          });
+          const result = [];
+          this.wb.SheetNames.forEach(sheetName => {
+            result.push({
+              sheetName: sheetName,
+              sheet: XLSX.utils.sheet_to_json(this.wb.Sheets[sheetName],
+                {
+                  head: 0,
+                  defval: " "
+                })
+            });
+          });
+          resolve(result);
+        };
+        reader.readAsBinaryString(file.raw);
+      });
+    },
+    importMerchantInfo() {
+
+    },
+    // 把excel用转化为table
+    excelToTable(data) {
+      const outData = JSON.parse(data.card);
+      // console.log("excel数据：  "+JSON.stringify(outData));
+
+      const tableHeadList = [];
+      let num = 1;
+      for (const tableHead2 in outData[0]) {
+        const tableHead1 = {};
+        tableHead1['column_name'] = "column_name" + num;
+        tableHead1['column_comment'] = tableHead2;
+        num = num + 1;
+        tableHeadList.push(tableHead1);
+      }
+      // console.log("表头数据："+JSON.stringify(tableHeadList));
+      this.tableHead = tableHeadList;//定制表头
+
+      const tableDataList = [];
+      for (let j = 0; j < outData.length; j++) {
+        const tableData1 = {};
+        for (let k = 0; k < tableHeadList.length; k++) {
+          // 第一行的数据
+          // console.log("表头字段："+tableHeadList[k]['column_name']+",表头数据：" + tableHeadList[k]['column_comment']);
+          for (const outDataKey in outData[j]) {
+            // console.log("outDataKey = " + outDataKey)
+            if (outData[j].hasOwnProperty(outDataKey)) {//对于（可能迭代异常（自定义/继承）成员，可能缺少 hasOwnProperty 检查）的错误的解决，用if语句判断
+              if (tableHeadList[k]['column_comment'] === outDataKey) {
+                tableData1[tableHeadList[k]['column_name']] = outData[j][outDataKey];
+                // console.log("tableHeadList[k]['column_comment'] = " + outData[j][outDataKey])
+              }
+            }
+          }
+        }
+        tableDataList.push(tableData1);
+      }
+      // console.log("tableHeadList");
+      // console.log(JSON.stringify(tableHeadList))
+      // console.log(tableHeadList);
+      // console.log("tableDataList");
+      // console.log(JSON.stringify(tableDataList))
+      // console.log(tableDataList);
+      this.tableData = tableDataList;//给表格内容赋值
+    },
+    //导出Excel
+    exportToExcel () {
+      let et = XLSX.utils.table_to_book(document.getElementById('tableData')); //此处传入table的DOM节点
+      let etOut = XLSX.write(et, {
+        bookType: 'xlsx',
+        bookSST: true,
+        type: 'array'
+      });
+      try {
+        FileSaver.saveAs(new Blob([etOut], {
+          type: 'application/octet-stream'
+        }), 'data.xlsx');   //trade-publish.xlsx 为导出的文件名
+      } catch (e) {
+        console.log(e, etOut) ;
+      }
+      return etOut;
     }
-  },
-  sendOption() {
-    this.$emit('sendOption', this.chartOption)
-  },
-  mounted() {
-    this.initChart();
   }
 }
+
 </script>
 
-<style>
+<style scoped>
 
 </style>

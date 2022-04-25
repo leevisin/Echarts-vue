1030dc016482d6b90fd2584fda1fc0d0c515172a
Dongsheng Li <Lds010@outlook.com>
2022-04-25 16:08:41 +0800

    delete Chart

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -10,7 +10,6 @@
     <el-menu-item v-for="(item, i) in navList" :key="i" :index="item.name">
       {{ item.navItem }}
     </el-menu-item>
-<!--    <a class="el-icon-menu" href="#nowhere" style="color:#222; float: right; padding: 20px">More Functions</a>-->
     <i class="el-col-menu" style="float: right; font-size: 45px; color: #222;padding-top: 8px"></i>
     <span style="position: absolute; padding-top: 20px; right: 43%; font-size: 20px; font-weight: bold">Data Visualization based on Echarts</span>
   </el-menu>
@@ -22,7 +21,6 @@ export default {
   data () {
     return {
       navList: [
-        // {name: '/chart', navItem: 'Chart'},
         {name: '/preview', navItem: 'Preview'},
         {name: '/data', navItem: 'Data'},
         {name: '/user', navItem: 'User'}
